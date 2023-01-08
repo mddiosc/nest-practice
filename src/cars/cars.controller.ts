26a50d6322edcacbc8875bf2dc8ff036cb83b275
mddiosc <mddiosc@gmail.com>
2023-01-08 13:41:15 +0100

    feat: add uuid and validation uuidpipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
@@ -20,7 +21,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
+  getCarById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.carsService.findObeById(id);
   }
 
