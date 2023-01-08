1d11eab2d3813723a13755da1280f46ad2ee8d51
Miguel Angel de Dios Calles <mddiosc@gmail.com>
2023-01-08 16:36:33 +0100

    feat: brands resources

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,59 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+
+@Injectable()
+export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Toyota',
+      createdAt: Date.now(),
+    },
+  ];
+
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = {
+      id: uuid(),
+      name: createBrandDto.name.toLowerCase(),
+      createdAt: new Date().getTime(),
+    };
+
+    this.brands.push(brand);
+    return brand;
+  }
+
+  findAll() {
+    return this.brands;
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) {
+      throw new NotFoundException(`Brand with id ${id} not found`);
+    }
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    const brand = this.findOne(id);
+    const updatedBrand = {
+      ...brand,
+      ...updateBrandDto,
+      id,
+      updatedAt: new Date().getTime(),
+    };
+    const index = this.brands.findIndex((brand) => brand.id === id);
+    this.brands[index] = updatedBrand;
+    return updatedBrand;
+  }
+
+  remove(id: string) {
+    const brand = this.findOne(id);
+    const index = this.brands.findIndex((brand) => brand.id === id);
+    this.brands.splice(index, 1);
+    return brand;
+  }
+}
