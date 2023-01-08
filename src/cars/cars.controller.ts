034702880b14a5c883479a6978d0c13572540043
mddiosc <mddiosc@gmail.com>
2023-01-08 12:55:55 +0100

    feat: pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -11,8 +11,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id') id: string) {
-    console.log({ id });
-    return this.carsService.findObeById(+id);
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findObeById(id);
   }
 }
